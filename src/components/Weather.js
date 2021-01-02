224cb17e071e0044a96bf6e5fe78e4d89cd3a6ba
Jay Moon <sjaymoon15@gmail.com>
2021-01-02 16:27:26 -0600

    Add Weather unit test.

--- src/components/Weather.js
+++ src/components/Weather.js
@@ -20,7 +20,11 @@ export default class Weather extends Component {
   render() {
     const { list } = this.props;
     return (
-      <ResponsiveContainer width='100%' aspect={2}>
+      <ResponsiveContainer
+        width='100%'
+        aspect={2}
+        data-test='component-weather'
+      >
         <LineChart data={list} margin={{ bottom: 5 }}>
           <CartesianGrid strokeDasharray='3 3' />
           <XAxis dataKey='dt_txt' />
